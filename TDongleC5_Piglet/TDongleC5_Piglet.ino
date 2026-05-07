4c981c029a1d19aa35addd705774544520ab0873
Hamspiced <hamspiced@gmail.com>
2026-05-06 23:26:36 -0400

    Bump version to v2.4

--- TDongleC5_Piglet/TDongleC5_Piglet.ino
+++ TDongleC5_Piglet/TDongleC5_Piglet.ino
@@ -40,7 +40,7 @@
 #include "esp_wifi.h"
 
 // Firmware version
-#define FIRMWARE_VERSION "v2.3"
+#define FIRMWARE_VERSION "v2.4"
 
 // ---------------- Pins (T-DONGLE C5) ----------------
 struct PinMap {
